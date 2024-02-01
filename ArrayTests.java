06894808a86d390aaf8777e74b111ada015c9cc0
KaleaPorter <kaleaporter24@gmail.com>
2024-02-01 11:07:32 -0800

    Lab3 CSE 15L

--- ArrayTests.java
+++ ArrayTests.java
@@ -3,16 +3,51 @@ import org.junit.*;
 
 public class ArrayTests {
 	@Test 
-	public void testReverseInPlace() {
+	public void testReverseInPlace1() {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  @Test
+  public void testReverseInPlace2() {
+    int[] input1 = {2, 3};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{3, 2}, input1);
+	}
 
   @Test
-  public void testReversed() {
+  public void testReverseInPlace3() {
+    int[] input1 = {1, 4, 7, 9};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{9, 7, 4, 1}, input1);
+	}
+
+  @Test
+  public void testReversed1() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReversed2() {
+    int[] input1 = {1, 2, 3};
+    assertArrayEquals(new int[]{3, 2, 1}, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversed3() {
+    int[] input1 = {2, 2, 2};
+    assertArrayEquals(new int[]{2, 2, 2}, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversed4() {
+    int[] input1 = {5};
+    assertArrayEquals(new int[]{5}, ArrayExamples.reversed(input1));
+  }
+  
 }
+
+
+
